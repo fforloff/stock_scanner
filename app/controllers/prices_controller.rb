b480a3b1e04edb1ee4a35529c6f83fc4acc807fd
VICTOR ORLOV <vint@VICTORs-MacBook-Air.local>
2015-10-12 19:07:05 +1100

    disable unused methods in the prices controller

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,74 +1,69 @@
 class PricesController < ApplicationController
-  before_action :set_price, only: [:show, :edit, :update, :destroy]
+  before_action :set_price, only: [:show]
+  #before_action :set_price, only: [:show, :edit, :update, :destroy]
 
   def index
     @e_ids = params[:entity_id]
     @e_ids = [@e_ids] if @e_ids.is_a?(String)
     @prices = Price.where(entity_id: {'$in': @e_ids}) if @e_ids
-    respond_to do |format|  
-      #format.json { render json: @prices, 
-      #  only: [:company_id, :date, :open, :high, :low, :close, :volume] }
-      #format.json { render json: @prices }
-#      format.csv { render text: @prices.to_to_csv }
-#      format.xml { render xml: @prices, only: [:date, :open, :high, :low, :close, :volume] }
-#      format.html 
-      format.json 
-    end  
+    respond_to do |format|
+      format.json
+    end
   end
 
   # GET /prices/1
   # GET /prices/1.json
-  def show
-  end
+  #def show
+  #end
 
   # GET /prices/new
-  def new
-    @price = Price.new
-  end
+  #def new
+  #  @price = Price.new
+  #end
 
   # GET /prices/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /prices
   # POST /prices.json
-  def create
-    @price = Price.new(price_params)
+  #def create
+  #  @price = Price.new(price_params)
 
-    respond_to do |format|
-      if @price.save
-        format.html { redirect_to @price, notice: 'Price was successfully created.' }
-        format.json { render :show, status: :created, location: @price }
-      else
-        format.html { render :new }
-        format.json { render json: @price.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #  respond_to do |format|
+  #    if @price.save
+  #       format.html { redirect_to @price, notice: 'Price was successfully created.' }
+  #       format.json { render :show, status: :created, location: @price }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @price.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /prices/1
   # PATCH/PUT /prices/1.json
-  def update
-    respond_to do |format|
-      if @price.update(price_params)
-        format.html { redirect_to @price, notice: 'Price was successfully updated.' }
-        format.json { render :show, status: :ok, location: @price }
-      else
-        format.html { render :edit }
-        format.json { render json: @price.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @price.update(price_params)
+  #       format.html { redirect_to @price, notice: 'Price was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @price }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @price.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /prices/1
   # DELETE /prices/1.json
-  def destroy
-    @price.destroy
-    respond_to do |format|
-      format.html { redirect_to prices_url, notice: 'Price was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @price.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to prices_url, notice: 'Price was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
